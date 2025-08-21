a8adb1ef5969db259dd29924bc9b44d2fa0046d3
Techatrix <techatrix@mailbox.org>
2025-08-21 23:01:18 +0200

    update to zig 0.15.1

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
-const minimum_zig_version = "0.15.0-dev.1160+e43617e68";
+const minimum_zig_version = "0.15.1";
 
 pub fn build(b: *std.Build) void {
     comptime if (builtin.zig_version.order(std.SemanticVersion.parse(minimum_zig_version) catch unreachable) == .lt) {

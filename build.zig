2333a486075464d8f8c35dbac074eb266ab11fc0
Techatrix <techatrix@mailbox.org>
2025-09-12 16:07:56 +0200

    move code generation to src/codegen/

--- build.zig
+++ build.zig
@@ -27,7 +27,7 @@ pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
         .name = "lsp-codegen",
         .root_module = b.addModule("lsp-codegen", .{
-            .root_source_file = b.path("src/main.zig"),
+            .root_source_file = b.path("src/codegen/codegen.zig"),
             .target = b.graph.host,
         }),
     });
